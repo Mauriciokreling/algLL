0695c29ca15d011cbc0b959ef78a123cfe9f5217
Mauriciokreling <43751312+Mauriciokreling@users.noreply.github.com>
2018-10-08 09:34:50 -0300

    variaveis globais

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -1,8 +1,22 @@
 // Aula0810.cpp : Defines the entry point for the console application.
-//
+//comentario de ate uma linha
 
-#include "stdafx.h"
+/*
+comentario longo
+para mais de uma linha
+*/
 
+#include "stdafx.h" //biblioteca
+#include <stdio.h> 
+#include <stdlib.h>
+// biblioteca  STD= padrao, standart
+// biblioteca i/0 entrada e saida
+
+//Variaveis (Globais, visiveis em todo o codigo)
+int a; //inteiro
+float b; //real (ponto flutuante)
+double c; // real(maior precisao do numero)
+bool d; //booleano (verdadeiro/falso) (true/false)
 
 int main()
 {
