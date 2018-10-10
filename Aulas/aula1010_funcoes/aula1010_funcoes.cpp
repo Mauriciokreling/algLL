4229cc688fa3abadbf32160d31e8f57acd0ef0f1
Mauriciokreling <mauriciokreling10@gmail.com>
2018-10-10 10:36:46 -0300

    ç

--- Aulas/aula1010_funcoes/aula1010_funcoes.cpp
+++ Aulas/aula1010_funcoes/aula1010_funcoes.cpp
@@ -11,13 +11,33 @@ void HelloWorld()//funcao que nao tem retorno
 
 }
 
+int Soma2Valores(int a, int b)
+{
+	int c;
+	c = a + b;
+	return c; //retornar o valor que a funcao executou
+	//pode usar tambem "return a + b" para informacoes mais simples
+}
+int lervalorinteiro()
+{
+	int variavel;
+	printf("\n INFORME O PRIMEIRO VALOR");
+	scanf_s("%i", &variavel);
+		return variavel;
+}
+
 
 int main()
 
 {
+	
+	int primeirovalor = lervalorinteiro();
+	int segundovalor = lervalorinteiro();
 
 	HelloWorld();
+	int d = Soma2Valores(primeirovalor, segundovalor );//chamando a funcao 
+	printf("\Resultado: %i\n", d);//mostrar o resultado
+
 	system("pause");
     return 0;
 }
-
---
+++ projeto_calc1010/projeto_calc1010/projeto_calc1010.cpp
@@ -0,0 +1,11 @@
+// projeto_calc1010.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
